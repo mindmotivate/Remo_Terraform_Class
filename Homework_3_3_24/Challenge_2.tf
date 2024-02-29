bbf5295fc9d578d26830d52864be51d59642d963
mindmotivate <130941970+mindmotivate@users.noreply.github.com>
2024-02-28 21:43:21 -0500

    Update Challenge_2.tf

--- Homework_3_3_24/Challenge_2.tf
+++ Homework_3_3_24/Challenge_2.tf
@@ -1,3 +1,5 @@
+# List Variable with count:
+
 variable "user_roles" {
 description = "List of user roles"
 type = list(string)
@@ -29,4 +31,33 @@ value = local_file.user_role_file[2].content
 output "user_role_file_output4" {
 value = [for file in local_file.user_role_file : "${file.filename}: ${file.content}"]
 }
+
+
+# Map Variable with for_each:
+
+variable "feature_toggles" {
+description = "List of toggle features"
+type = map(string)
+default = {
+signup_enabled ="true"
+profile_editing_enabled = "false"
+search_functionality_enabled = "true"
+}
+
+}
+
+
+resource "local_file" "feature_toggle_list" {
+  for_each = var.feature_toggles
+  filename = "${path.module}feature-${each.key}.txt"
+  content = "${each.key}:${each.value}"
+}
+
+
+output "feature_toggle_list_output" {
+  value = [
+    for feature in local_file.feature_toggle_list : feature.content
+  ]
+}
+
 
