eab6b8de437805cadbcc2844e1def9720dc8f898
mindmotivate <130941970+mindmotivate@users.noreply.github.com>
2024-02-28 21:01:56 -0500

    Create Challenge_2.tf

---
+++ Homework_3_3_24/Challenge_2.tf
@@ -0,0 +1,31 @@
+variable "user_roles" {
+description = "List of user roles"
+type = list(string)
+default = ["admin","editor","viewer"]
+}
+
+
+resource "local_file" "user_role_file" {
+count =  length(var.user_roles)
+filename = "user_role-${var.user_roles[count.index]}.txt"
+content = "${var.user_roles[count.index]}"
+}
+
+output "user_role_file_output" {
+value = local_file.user_role_file[*].content
+}
+
+output "user_role_file_output1" {
+value = local_file.user_role_file[0].content
+}
+output "user_role_file_output2" {
+value = local_file.user_role_file[1].content
+}
+
+output "user_role_file_output3" {
+value = local_file.user_role_file[2].content
+}
+
+output "user_role_file_output4" {
+value = [for file in local_file.user_role_file : "${file.filename}: ${file.content}"]
+}
