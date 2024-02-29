c65bb536cd279e09c880dc44bf73ee8eb0f1a494
mindmotivate <130941970+mindmotivate@users.noreply.github.com>
2024-02-28 21:12:55 -0500

    Update Challenge_2.tf

--- Homework_3_3_24/Challenge_2.tf
+++ Homework_3_3_24/Challenge_2.tf
@@ -7,7 +7,7 @@ default = ["admin","editor","viewer"]
 
 resource "local_file" "user_role_file" {
 count =  length(var.user_roles)
-filename = "user_role-${var.user_roles[count.index]}.txt"
+filename = "${path.module}user_role-${var.user_roles[count.index]}.txt"
 content = "${var.user_roles[count.index]}"
 }
 
@@ -29,3 +29,4 @@ value = local_file.user_role_file[2].content
 output "user_role_file_output4" {
 value = [for file in local_file.user_role_file : "${file.filename}: ${file.content}"]
 }
+
